d2cd4629d6ea4313eb95a6d5abc1a9ec25247fc4
F Hersi <33755306+fhersi@users.noreply.github.com>
2022-05-21 16:33:08 +0100

    fix bug - first zero on display

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const btnNumbers = document.querySelectorAll("button.numbers");
-
 const display = document.querySelector("span.display");
 
 for (let i = 0; i < btnNumbers.length; i++) {
-	btnNumbers[i].addEventListener("click", function () {
-		display.innerText += btnNumbers[i].innerText;
+	btnNumbers[i].addEventListener("click", (e) => {
+		display.innerText == 0
+			? (display.innerText = btnNumbers[i].innerText) // clear the first zero
+			: (display.innerText += btnNumbers[i].innerText); // concat numbers
 	});
 }
 
